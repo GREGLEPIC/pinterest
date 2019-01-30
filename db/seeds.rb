e124a96a233e99a6dc4f920b85508053cf913be0
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-31 00:01:55 +0100

    FINISH

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+    user = User.create!(name: Faker::Name.first_name)
+  end
+
+  10.times do
+    pin = Pin.create!(url: Faker::Internet.url, user_id: Faker::Number.between(1, 10))
+  end
+
+  10.times do
+    comment = Comment.create!(text: Faker::ChuckNorris.fact, user_id: Faker::Number.between(1, 30), pin_id: Faker::Number.between(1, 20))
+  end
